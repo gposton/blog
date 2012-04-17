0ac55789f76f013964624f0f10c4de729cd6bcd7
gposton <gposton1040@gmail.com>
2012-04-17 02:35:52 -0400

    removed deprication warnings (Rails 3 upgrade)

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,27 +1,22 @@
 class Notifier < ActionMailer::Base
 
+  default :from => 'Blog <gposton1040@gmail.com>', :to => 'gposton1040@gmail.com'
+
   def new_comment(comment, object)
-    recipients    'gposton1040@gmail.com'
-    from          'Blog <gposton1040@gmail.com>'
-    subject       '[Blog] New comment'
-    content_type  'text/html'
-    body          :comment => comment, :object => object
+    @comment = comment
+    @object = object
+    mail(:subject => '[Blog] New comment', :content_type => 'text/html')
   end
 
   def new_photo(photo)
-    recipients    'gposton1040@gmail.com'
-    from          'Blog <gposton1040@gmail.com>'
-    subject       '[Blog] New photo'
-    content_type  'text/html'
-    body          :photo => photo
+    @photo = photo
+    mail(:subject => '[Blog] New photo', :content_type => 'text/html' )
   end
 
   def new_tournament(tournament, recent_tournaments)
-    recipients    User.all.collect{|user|user.email}.join(', ')
-    from          'Poker <gposton1040@gmail.com>'
-    subject       'Poker!!!'
-    content_type  'text/html'
-    body          :tournament => tournament, :recent_tournaments => recent_tournaments
+    @tournament = tournament
+    @recent_tournaments = recent_tournaments
+    mail(:to => User.all.collect{|user|user.email}.join(', '), :from => 'Poker <gposton1040@gmail.com>', :subject => 'Poker!!!', :content_type => 'text/html')
   end
 
 end
