9f990f633c79b7ee67a6b439294aaeb3592ea527
gposton <gposton1040@gmail.com>
2012-06-11 01:36:21 -0400

    default host for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Blog::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "http://www.glennposton.com" }
 
   # Enable threaded mode
   # config.threadsafe!
