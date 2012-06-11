1717f89914a7eaa69470fedf06a5ec149e74b582
gposton <gposton1040@gmail.com>
2012-06-10 20:23:13 -0400

    mongrel logfile and pid

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,9 +32,9 @@ namespace :deploy do
  task :stop do ; end
  task :restart, :roles => :app, :except => { :no_release => true } do
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   run "cd #{release_path} && bundle exec mongrel_rails start -p 8000 -e production -d"
-   run "cd #{release_path} && bundle exec mongrel_rails start -p 8001 -e production -d"
-   run "cd #{release_path} && bundle exec mongrel_rails start -p 8002 -e production -d"
+   run "cd #{release_path} && bundle exec mongrel_rails start -p 8000 -e production -d -l /var/www/blog/shared/logs/mongrel.8000.log -p /var/www/blog/shared/pids/mongrel.8000.pid"
+   run "cd #{release_path} && bundle exec mongrel_rails start -p 8001 -e production -d -l /var/www/blog/shared/logs/mongrel.8001.log -p /var/www/blog/shared/pids/mongrel.8001.pid"
+   run "cd #{release_path} && bundle exec mongrel_rails start -p 8002 -e production -d -l /var/www/blog/shared/logs/mongrel.8002.log -p /var/www/blog/shared/pids/mongrel.8002.pid"
  end
  desc "Update the crontab file"  
  task :update_crontab, :roles => :db do  
