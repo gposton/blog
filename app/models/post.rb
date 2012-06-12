3d599e7247421402b371594facc4c467781db002
gposton <gposton1040@gmail.com>
2012-06-11 21:22:59 -0400

    fix bug in sql when creating new post

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,7 +15,7 @@ class Post < ActiveRecord::Base
     self.web_title = self.title.downcase.gsub(' ', '_').gsub(/\W/,'')
     # make sure that the web_title is unique
     # get everything that has a similar web_title
-    previous_posts_with_same_web_title = Post.find_by_sql "Select * from Posts where web_title like '#{web_title}%'"
+    previous_posts_with_same_web_title = Post.find_by_sql "Select * from posts where web_title like '#{web_title}%'"
     # append the new web title with max_id + 1 (note the 'order by' in the sql above)
     self.web_title = "#{self.web_title}_#{previous_posts_with_same_web_title.size}" unless previous_posts_with_same_web_title.empty?
   end
