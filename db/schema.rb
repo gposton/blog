af3e7770299d96fcc2cba72140a7a9226eee2101
gposton <gposton1040@gmail.com>
2012-06-21 18:11:01 -0400

    fix posts timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611030732) do
+ActiveRecord::Schema.define(:version => 20120621220352) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -21,12 +21,12 @@ ActiveRecord::Schema.define(:version => 20120611030732) do
 
   create_table "comments", :force => true do |t|
     t.string   "title",            :limit => 50, :default => ""
-    t.text     "content",                        :default => ""
     t.integer  "commentable_id"
     t.string   "commentable_type"
     t.integer  "user_id"
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
+    t.text     "content"
   end
 
   add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
