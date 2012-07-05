38308744b66d1b282447ef36b291a98ccc9490a4
Glenn Poston <gposton1040@gmail.com>
2012-07-05 07:07:56 -0400

    picture frame is working, re-enabled tab

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,9 +10,16 @@ class Photo < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => ['image/gif', 'image/jpeg', 'image/png']
 
   has_attached_file :image,
-                    :styles          => { :large => "1280x800", :medium => "700x400", :thumb => "100x60" },
-                    :convert_options => { :all => '-auto-orient' },
-                    :url             => "/system/images/uploaded/:style/:basename.:extension",
-                    :path            => ":rails_root/public/system/images/uploaded/:style/:basename.:extension"
+    :storage             => :s3,
+    :bucket              => S3_BUCKET_NAME,
+    :s3_credentials      => {
+      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
+    :styles              => {
+      :large             => "1280x800",
+      :medium            => "700x400",
+      :thumb             => "100x60"},
+    :convert_options     => { :all => '-auto-orient' },
+    :path                => ":style/:basename.:extension"
 
 end
